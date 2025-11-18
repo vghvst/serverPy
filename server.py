77c1938291f1371e8855d7afc8d1a72ea5dca24e
vghvst <aghastcreatorsexperimental495@gmail.com>
2025-11-18 22:25:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import os
 from g4f.api import run_api
 
 port = int(os.environ.get("PORT", 8080))
-run_api(host="0.0.0.0", port=port, openai=True)  # <-- це ключове
+run_api(host="0.0.0.0", port=port)
