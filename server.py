bd7b56b7a0b9ffd0309476e2777094ba3de2314d
vghvst <aghastcreatorsexperimental495@gmail.com>
2025-12-11 20:11:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import os
+import g4f
 from g4f.api import run_api
 
 g4f.Provider.ProviderList.pop("PuterJS", None)
 
 port = int(os.environ.get("PORT", 8080))
 run_api(host="0.0.0.0", port=port)
+
 
